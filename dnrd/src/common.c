e364e99aa3a88cd5a8e5a8857d27d28ef4ccfcc8
Natanael Copa <n@tanael.org>
2004-04-09 09:55:31 +0000

    fixed so 'make distcheck' works.

--- dnrd/src/common.c
+++ dnrd/src/common.c
@@ -18,7 +18,10 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#include "config.h"
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include "common.h"
 #include <stdarg.h>
 #include <unistd.h>
