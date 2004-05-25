531d71e3a81b847ceaf5e9ad896741c4cf002dab
Natanael Copa <n@tanael.org>
2004-05-25 09:33:20 +0000

    This commit was generated by cvs2svn to compensate for changes in r54, which included commits to RCS files with non-trunk default branches.

---
+++ www/footer.php
@@ -0,0 +1,5 @@
+<div id="footer">
+<p>Copyright &copy; 2004 Natanael Copa</p>
+</div>
+</body>
+</html>
---
+++ www/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+
+<html>
+<head>
+<title>DNRD - Domain Name Relay Daemon</title>
+<link href="dnrd.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
