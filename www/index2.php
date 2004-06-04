3adc858424d125015395e6c8b47692bb67c19149
Natanael Copa <n@tanael.org>
2004-06-04 15:36:25 +0000

    typos

--- www/index2.php
+++ www/index2.php
@@ -8,25 +8,34 @@ require("menu.php");
 
 <? menu("Home"); ?>
 
-
 <div id="content">
+
 <h2><a name="about">What DNRD is</a></h2>
+
 <p>
 Domain Name Relay Daemon is a caching, forwarding DNS proxy
-server. Most useful on vpn or dialup firewalls but is a nice DNS cache
+server. Most useful on vpn or dialup firewalls but it is also a nice DNS cache
 for minor networks and workstations.
+</p>
+
 <h3>Features</h3>
+
 <ul>
-<li>Caching of DNS requests</li>
-<li>Support for backup DNS servers</li>
+
+<li>Caching of DNS requests.</li>
+
+<li>Support for backup DNS servers.</li>
+
 <li>Support for simple routing - specify different forward DNS servers
-  for different domains. This great for private networks, vpn's and
-  offline sites.</li>
+  for different domains.</li>
+
 <li>Force authorative or unauthorative answers for specified
-domains. Can be used as a very simple dns server for home
-networks</li>
-<li>share the /etc/hosts over the network.</li>
-<li>support for openbsd, freebsd and linux</li>
+domains.</li>
+
+<li>Share the /etc/hosts over the network.</li>
+
+<li>Support for openbsd, freebsd and linux.</li>
+
 </ul>
 
 </div>
